fa6b58348286f4a31535ba19c9e512d0018143a0
Uvindu Kodikara <153884375+uvinduuu@users.noreply.github.com>
2024-05-31 17:40:08 +0530

    Update mycode1.cpp

--- OTA/mDash/mycode1.cpp
+++ OTA/mDash/mycode1.cpp
@@ -1,1 +1,21 @@
+#define MDASH_APP_NAME "MinimalApp"
+#include <mDash.h>
 
+#include <WiFi.h>
+
+#define WIFI_NETWORK "SSID"
+#define WIFI_PASSWORD "Password"
+#define DEVICE_PASSWORD "ID_from_mDash"
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  motor_setup();
+  Serial.begin(115200);
+  WiFi.begin(WIFI_NETWORK, WIFI_PASSWORD);
+  mDashBegin(DEVICE_PASSWORD);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
